028a9cb7db1205fb98a42fc1e53b5e2320d3d1ff
96sanjay <sanjaysajeev1996@gmail.com>
2026-06-16 09:31:15 +0200

    chore: remove deleted cmdp_env_shield import from train_omnisafe.py

--- scripts/training/train_omnisafe.py
+++ scripts/training/train_omnisafe.py
@@ -4,7 +4,6 @@ import argparse, yaml
 import omnisafe
 
 import citylearn_safe.cmdp_env  # <-- IMPORTANT: triggers @env_register (registers CityLearnSafety-V2G-v2)
-import citylearn_safe.cmdp_env_shield  # V2-shield: ActionProjection + CMDP
 
 def main(cfg_path: str) -> None:
     cfg = yaml.safe_load(open(cfg_path, "r"))
